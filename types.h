845e1a1a232337476df5c2c901dce4240fcdc189
Pavol Plaskon <xplask00@stud.fit.vutbr.cz>
2015-10-07 20:45:02 +0200

    add token struct in types.h
    Signed-off-by: Pavol Plaskon <xplask00@stud.fit.vutbr.cz>

---
+++ types.h
@@ -0,0 +1,28 @@
+/*
+ * Course IFJ @ FIT VUT Brno, 2015
+ * IFJ15 Interpreter Project
+ *
+ * Authors:
+ * Lukas Osadsky  - xosads00
+ * Pavol Plaskon  - xplask00
+ * Pavel Pospisil - xpospi88
+ * Matej Postolka - xposto02
+ *
+ * Unless otherwise stated, all code is licensed under a
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ */
+
+#include "enums.h"
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef _token
+{
+    TToken_type type;
+    char *data;
+
+}TToken;
+
+#endif //TYPES_H

c97f20efbdb9659d57c0ef27702d27d6b97f6989
Pavol Plaskon <xplask00@stud.fit.vutbr.cz>
2015-10-14 18:51:59 +0200

    added stack library

---
+++ stack.c
@@ -0,0 +1,70 @@
+/*
+ * Course IFJ @ FIT VUT Brno, 2015
+ * IFJ15 Interpreter Project
+ *
+ * Authors:
+ * Lukas Osadsky  - xosads00
+ * Pavol Plaskon  - xplask00
+ * Pavel Pospisil - xpospi88
+ * Matej Postolka - xposto02
+ *
+ * Unless otherwise stated, all code is licensed under a
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ */
+
+#include<stdlib.h>
+
+#include "stack.h"
+#include "enums.h"
+
+int stack_init(TStack *stack)
+{
+    stack->data = malloc(DEFAULT_STACK_SIZE * sizeof(void *));
+    if(stack->data == NULL)
+         return E_ALLOC;   
+    
+    stack->capacity = DEFAULT_STACK_SIZE;
+    stack->used = 0;
+
+    return 0;
+}
+
+void stack_free(TStack* stack)
+{
+    free(stack->data);    
+}
+
+int stack_push(TStack *stack, void* item)
+{
+    void **new_data;
+
+    if(stack->capacity == (stack->used + 1))
+    {
+        new_data = realloc(stack->data, 
+                        sizeof(void *) * stack->capacity*VECTOR_AUTORESIZE_COEF);    
+        if(new_data == NULL)
+            return E_ALLOC;
+        stack->data = new_data;
+        stack->capacity = stack->capacity * VECTOR_AUTORESIZE_COEF;
+    }
+
+    stack->data[stack->used] = item;
+    (stack->used)++;             
+
+    return 0;
+}
+
+void stack_pop(TStack *stack)
+{
+    if(stack->used > 0)
+        (stack->used)--;
+}
+
+void* stack_top(TStack *stack)
+{
+    if(stack->used > 0)
+        return stack->data[stack->used - 1];
+    else
+        return NULL; 
+}
---
+++ stack.h
@@ -0,0 +1,34 @@
+/*
+ * Course IFJ @ FIT VUT Brno, 2015
+ * IFJ15 Interpreter Project
+ *
+ * Authors:
+ * Lukas Osadsky  - xosads00
+ * Pavol Plaskon  - xplask00
+ * Pavel Pospisil - xpospi88
+ * Matej Postolka - xposto02
+ *
+ * Unless otherwise stated, all code is licensed under a
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ */
+
+#ifndef STACK_H
+#define STACK_H
+
+#define DEFAULT_STACK_SIZE 15
+
+typedef struct Stack
+{
+    void **data; 
+    int capacity;
+    int used;
+} TStack;
+
+int stack_init(TStack* stack);
+void stack_free(TStack* stack);
+void stack_pop(TStack* stack);
+int stack_push(TStack* stack, void* item);
+void* stack_top(TStack* stack);
+
+#endif //STACk_H

299c1afc05361ac5eb613ff6cd5a494673d1cdcd
Matej Postolka <xposto02@stud.fit.vutbr.cz>
2015-10-15 10:31:44 +0200

    Added manual for Doxygen (in README) and fully documented string.h
    Signed-off-by: Matej Postolka <xposto02@stud.fit.vutbr.cz>

--- string.h
+++ string.h
@@ -13,20 +13,81 @@
  *
  */
 
+/**
+  * @file string.h
+  * @brief String library
+  *
+  * This library allows the use of dynamically sized strings with automatic
+  * extension.
+  *
+  * @warning You may print the string using printf("%s\n", 
+  * s->data), but you MUST insert a binary 0 at the end of the string!
+  */
+
 #ifndef VECTOR_H
 #define VECTOR_H
 
+/**
+  * @brief TString structure
+  *
+  * Structure which holds the dynamic string.
+  */
 typedef struct
 {
-  char *data;
-  int capacity;
-  int used;
+  char *data; /**< Points to the location of the string in memory. */
+  int capacity; /**< Total capacity */
+  int used; /**< Bytes used */
 } TString;
 
+/**
+  * @brief String intialization function
+  *
+  * Initializes an empty string structure @p *v with an initial
+  * size of @p size. Returns an @c error code (see enums.h).
+  *
+  * @param *v Pointer to the string struct
+  * @param size Initial string size (you may use STR_AUTORESIZE_COEF)
+  */
 int initString(TString *v, int size);
+
+/**
+  * @brief Insert a new character into the string
+  *
+  * Inserts a new character into the string.
+  *
+  * @param *v Pointer to the string struct
+  * @param value Char to insert
+  */
 int insertIntoString(TString *v, char value);
+
+/**
+  * @brief Get specific char from string
+  * 
+  * Safely retrieve a character from the string
+  *
+  * @param *v Pointer to the string struct
+  * @param index Index of character to retrieve;
+  */
 int getFromString(TString *v, int index);
+
+/**
+  * @brief Delete characters from string
+  *
+  * Safely remove characters from the string
+  *
+  * @param *v Pointer to the string struct
+  * @param index Index at which to start deletion
+  * @param count Number of characters to delete
+  */
 void deleteFromString(TString *v, int index, int count);
+
+/**
+  * @brief Deallocate string
+  *
+  * Deallocate string
+  *
+  * @param *v Pointer to the string
+  */
 void freeString(TString *v);
 
 #endif

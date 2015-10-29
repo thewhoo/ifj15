ea6f61b3060d892cec4f0cd066961b975ae5646c
Pavol Plaskon <xplask00@stud.fit.vutbr.cz>
2015-10-29 15:09:18 +0100

    interpret: enum of first basic instructions

---
+++ interpret.h
@@ -0,0 +1,56 @@
+/*
+ * Course IFJ @ FIT VUT Brno, 2015
+ * IFJ15 Interpreter Project
+ *
+ * Authors:
+ * Lukas Osadsky  - xosads00
+ * Pavol Plaskon  - xplask00
+ * Pavel Pospisil - xpospi88
+ * Matej Postolka - xposto02
+ *
+ * Unless otherwise stated, all code is licensed under a
+ * Creative Commons Attribution-ShareAlike 4.0 International License
+ *
+ */
+
+/**
+  * @file interpret.h
+  * @brief Interpret for 3-address code.
+  *
+  */
+
+#ifndef INTERPRET_H
+#define INTERPRET_H
+
+enum instructions
+{
+    INS_ASSIGN,
+    INS_ADD,
+    INS_SUB,
+    INS_MUL,
+    INS_DIV,
+    INS_EQ,
+    INS_NEQ,
+    INS_GREATER,
+    INS_GREATEQ
+    INS_LESSER,
+    INS_LESSEQ,
+    INS_JMP,
+    INS_CJMP,
+    INS_LAB,
+    INS_CALL,
+    INS_PUSH_TAB,
+    INS_POP_TAB,
+    //built-int functions
+    INS_LENGTH,
+    INS_SUBSTR,
+    INS_CONCAT,
+    INS_FIND,
+    INS_SORT,
+    INS_CIN,
+    INS_COUT
+}; 
+
+void interpret();
+
+#endif

b7d97b41a113029e8adb1a85bcb1ecd2ce3e9419
Pavel Pospíšil <xpospi88@stud.fit.vutbr.cz>
2015-11-18 12:53:20 +0100

    expr: oprava generovani instrukci
    Signed-off-by: Pavel Pospíšil <xpospi88@stud.fit.vutbr.cz>

--- expr.c
+++ expr.c
@@ -335,7 +335,7 @@ void generate_code()
 	TVariable *var_to_push;
 
 	postfix_count_test();
-	while (gene_stack->used > 1) {
+	while (!stack_empty(gene_stack)) {
 		tok = stack_top(gene_stack);
 		stack_pop(gene_stack);
 		if (token_is_operand(tok)) {
@@ -352,7 +352,6 @@ void generate_code()
 			stack_push(ins_stack, G.g_return);
 		}
 	}
-	printf("%d!!!\n", ins_stack->used);
 	actual_ins = create_ins(INS_ASSIGN, expr_var, stack_top(ins_stack), NULL);
 	list_insert(actual_ins_list, actual_ins);
 }

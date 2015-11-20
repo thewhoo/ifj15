476904ac667ccdc65ca2a05a6afed24ff9cf5311
Pavol Plaskon <xplask00@stud.fit.vutbr.cz>
2015-11-20 23:18:59 +0100

    tests: script tests all tests
    tests without errors - 2 failed - function calls
    
    Signed-off-by: Pavol Plaskon <xplask00@stud.fit.vutbr.cz>

--- test.py
+++ test.py
@@ -3,10 +3,33 @@
 import os
 import subprocess
 import sys
+import random
 
-def print_test_head(source):
+def print_head(text):
+    print("-------------------------------------")
+    print("-------------------------------------")
+    print("\n{}\n".format(text))
+    print("-------------------------------------")
+
+
+def print_test_head(n, source):
     print("-------------------------------")
-    print("TEST {}: {}".format(x, source))
+    print("TEST {}: {}".format(n, source))
+
+
+def set_cin_cout(source):
+    if 'cin_string' in source:
+        ret = ''.join([chr(random.randint(33, 126)) for i in
+            range(random.randint(0,10))])
+        return ret, ret
+    if 'cin_int' in source:
+        ret = random.randint(0, 1000000)
+        return str(ret), str(ret)
+    if 'cin_double' in source:
+        ret = random.uniform(0.0, 1000000)
+        ret_str = '{:g}'.format(ret)
+        return ret_str, ret_str
+    return '', ''
 
 
 def check_ret_code(returned, expected):
@@ -19,10 +42,10 @@ def check_ret_code(returned, expected):
         return False
 
 
-def check_stdout(out):
-    if out.strip():
+def check_stdout(out, expected):
+    if out != expected:
         print("UNexpected output: "+out)
-        print(out)
+        print("Expected: "+expected)
         return False
     return True
 
@@ -33,13 +56,12 @@ def check_stderr(err):
         return False
     return True
 
-def print_statistics():
+def print_statistics(n, n_ok, n_fail, fails):
     print("-------------------------------")
-    print("-------------------------------")
-    print("Number of tests: {}\nSuccess tests: {}\nFailed: {}\n"
-          .format(x, x_ok, x_fail))
+    print("Number of tests: {}\nSucceed tests: {}\nFAILED: {}\n"
+          .format(n, n_ok, n_fail))
 
-    if x_fail != 0:
+    if n_fail != 0:
         print("Failed tests:")
         for k, v in sorted(fails.items()):
             print("TEST {}: {}".format(k,v))
@@ -48,34 +70,37 @@ if not(os.path.isfile('ifj') and os.access('ifj', os.X_OK)):
     print("No executable file found, try 'make'.")
     sys.exit()
 
+#################################################
+#                    OK
+#################################################
 
-tests_dir = os.path.join(os.getcwd(), 'tests')
+tests_dir = os.path.join(os.getcwd(), 'tests/tests_ok')
 
 x = 0
 x_ok = 0
 x_fail = 0
 fails = {}
 
-print("-------------------------------------")
-print("-------------------------------------")
-print("\nIFJ15: Tests containing some error.\n")
-print("-------------------------------------")
+print_head("IFJ15: Tests without errors.")
 
 for source in [f for f in os.listdir(tests_dir)
-              if os.path.isfile(os.path.join(tests_dir, f))]:
+              if not f.endswith('.out')]:
 
     x = x + 1
     source_path = os.path.join(tests_dir, source)
     proc = subprocess.Popen(["./ifj", source_path],
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
+
+    expected_out = open(source_path + '.out', 'r').read()
     out, err = proc.communicate()
+    
     ret_code = proc.returncode
     
-    print_test_head(source)
-    c_ret = check_ret_code(ret_code, source[-1])
-    c_out = check_stdout(out.decode('utf-8'))
-    c_err = check_stderr(err.decode('utf-8'))
+    print_test_head(x, source)
+    c_ret = check_ret_code(ret_code, 0)
+    c_out = check_stdout(out.decode('utf-8'), expected_out)
+    c_err = err is not True
 
     if c_ret and c_out and c_err:
         x_ok = x_ok + 1
@@ -83,4 +108,47 @@ for source in [f for f in os.listdir(tests_dir)
         x_fail = x_fail + 1
         fails[x] = source
 
-print_statistics()
+
+##################################################
+#                 Error tests
+##################################################
+
+tests_dir = os.path.join(os.getcwd(), 'tests')
+
+fx = 0
+fx_ok = 0
+fx_fail = 0
+ffails = {}
+
+
+print_head("IFJ15: Tests containing some errors.")
+
+for source in [f for f in os.listdir(tests_dir)
+              if os.path.isfile(os.path.join(tests_dir, f))]:
+
+    fx = fx + 1
+    source_path = os.path.join(tests_dir, source)
+    proc = subprocess.Popen(["./ifj", source_path],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    ret_code = proc.returncode
+    
+    print_test_head(fx, source)
+    c_ret = check_ret_code(ret_code, source[-1])
+    c_out = check_stdout(out.decode('utf-8'), '')
+    c_err = check_stderr(err.decode('utf-8'))
+
+    if c_ret and c_out and c_err:
+        fx_ok = fx_ok + 1
+    else:
+        fx_fail = fx_fail + 1
+        ffails[fx] = source
+
+print("---------------------------------")
+print("------------RESULTS--------------")
+print("Tests without errors:")
+print_statistics(x, x_ok, x_fail, fails)
+print("---------------------------------")
+print("Tests with some error:")
+print_statistics(fx, fx_ok, fx_fail, ffails)
